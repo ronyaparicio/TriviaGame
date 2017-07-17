d4271ade8fbe7ae4730be9a348cd22f21af7f474
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-07-17 16:36:07 -0600

    a

--- assets/javascreipt/app.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function() {
-
-  $("#start").on("click", function() {
-    $("#start").hide(1000);
-    $(".game").show(1000);
-  });
-
-});
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,26 @@
 $('document').ready(function() {
+  var questions = [
+    "PUT THE PLANETS IN ORDER FROM THE SUN?",
+    "HOW MANY MILES ARE IN A LIGHT YEAR?",
+    "WHAT'S THE NAME OF EARTH'S MOON?",
+    "HOW FAR IS EARTH FROM THE SUN?",
+    "WHICH IS NOT A TYPE OF STAR?",
+    "WHICH STAR IS THE BRIGHTEST?",
+    "DEFINE A METEOR.",
+    "WHO PROPOSED THE 3 LAWS OF PLANETARY MOTION?",
+  ];
 
   $("#start").on("click", function() {
     $("#start").hide(1000);
     $(".game").show(1000);
+    $(".text").show(1000);
+
+    //Pick a questions a answers.
+    for (var i = 0; i < questions.length; i++) {
+      
+    }
+
+
   });
 
 });
